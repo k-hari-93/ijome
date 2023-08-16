838b7e2339688925e4f0d1674cb2d60f31045da2
Hari Kizhedath <hari@mail.uni-paderborn.de>
2023-08-16 12:17:19 +0200

    add post creator and post viewers, style changes, next image

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,8 +1,41 @@
-import { z } from "zod";
+import { clerkClient } from "@clerk/nextjs";
+import type { User } from "@clerk/nextjs/dist/types/server";
+import { TRPCError } from "@trpc/server";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+const filterUserFieldsForClient = (user: User) => {
+  return { id: user.id, username: user.username, profileImage: user.imageUrl };
+};
+
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+    });
+
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((post) => post.authorId),
+        limit: 100,
+      })
+    ).map(filterUserFieldsForClient);
+
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.authorId);
+
+      if (!author?.username)
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Could not find the author of the post",
+        });
+
+      return {
+        post,
+        author: {
+          ...author,
+          username: author.username,
+        },
+      };
+    });
   }),
 });
